22e9baf2f57babb2732461572614d144d00fca7e
Andrea <andry.tino@gmail.com>
2017-03-01 12:35:00 +0100

    More code and styling

--- src/jesonmor.js
+++ src/jesonmor.js
@@ -3,32 +3,85 @@
  * Andrea Tino - 2017
  */
 
+/**
+ * The Jeson Mor object.
+ * 
+ * Notes:
+ * - Indexes are 1-based.
+ */
 var jm = function() {
+    /**
+     * The horse piece.
+     */
+    var Horse = function() {
+        // Construct the object
+        var i = 1;
+        var j = 1;
+
+        // Object public interface
+        return {
+
+        };
+
+        function _move
+    }; // Horse
+
     /**
      * The board object.
      */
     var Board = function(_size) {
         var CONTAINER_CLASSNAME = "container";
         var HOUSE_CLASSNAME = "house";
+        var HOUSE_HIGHLIGHTED_CLASSNAME = "highlighted";
+
+        // Lazy initialized variables
+        var houses = null;
 
         // Construct object
         var size = _validateSize(_size);
 
         // Object public interface
         return {
-            build: _build
+            build: _build,
+            dispose: _clean
         };
+
+        function _highlight(i, j) {
+            var house = _getHouse(i, j);
+            if (!house) return;
+
+            house.classList.add(HOUSE_HIGHLIGHTED_CLASSNAME);
+        }
+
+        function _unhighlight(i, j) {
+            var house = _getHouse(i, j);
+            if (!house) return;
+
+            house.classList.remove(HOUSE_HIGHLIGHTED_CLASSNAME);
+        }
+
+        function _getHouse(i, j) {
+            if (!houses) return null;
+
+            return houses[i + ":" + j];
+        }
 
         function _build() {
             var dimension = size * size;
+            houses = {}; // Initializing dictionary
 
             var container = _createHouse();
             container.className = CONTAINER_CLASSNAME;
 
-            for (var i = 0; i < dimension; i++) {
+            for (var k = 0; k < dimension; k++) {
                 var house = _createHouse();
                 house.className = HOUSE_CLASSNAME;
                 container.appendChild(house);
+
+                // Index houses
+                var i = Math.ceil(k / size);
+                var j = k % size;
+                houses[i + ":" + j] = house;
             }
 
             document.body.appendChild(container);
@@ -36,6 +89,20 @@ var jm = function() {
 
         function _createHouse() {
             return document.createElement("div");
+        }
+
+        function _isBuilt() {
+            return houses != null;
+        }
+
+        function _clean() {
+            if (!_isBuilt()) return;
+
+            var container = houses["1:1"].parentElement;
+            if (!container) return;
+
+            container.remove();
+            houses = null;
         }
 
         function _validateSize(size) {
